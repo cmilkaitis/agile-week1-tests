173b624f652246081241201df22d55c83d4e1d85
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-06-06 14:26:35 -0400

    Added display stock function and function to check Uppercase on Ticker

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 
-
+// TEST PAGE IN jQUERY
 $(document).ready(function() {
-    let arr = []; 
+    let stocks = []; 
     
     $("#btn").click(function() {
         let term = $("#search-text").val();
-        arr.push(term);
+        stocks.push(term);
         $("#content").append(`<p>${term}</p>`);
         $("#search-text").val("");
     }); 
-    console.log(arr);
+    console.log(stocks);
 });
+
+
+    // FUNCTIONS FOR PROJECT BELOW
+
+    // Sets ticker value toUpperCase if the are not already
+    const upperCaseTicker = array => {
+        array.forEach(stock => {
+            if (stock.ticker !== stock.ticker.toUpperCase()) {
+                stock.ticker = stock.ticker.toUpperCase();
+            }   
+        });
+        return array;
+    }
+
+    console.log(upperCaseTicker(stocks));
+
+
+    // Displays Stock to the content div
+    const displayStocks = array => {
+        array.forEach(stock => {
+            $("#content").append(`<div style="border-bottom: 1px black solid"><p>Ticker: ${stock.ticker}</p> <p>Price: ${stock.price}</p> <p>Owned by: ${stock.ownedBy}</p></div>`);
+        })
+    }
+
+    displayStocks(stocks);
